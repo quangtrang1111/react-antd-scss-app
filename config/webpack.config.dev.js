dfbf7305613357904025bfca7a488a227c7cf751
QuangTrang <thuynh@2clicksolutions.onmicrosoft.com>
2018-04-10 17:51:28 +0700

    scss

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -186,23 +186,38 @@ module.exports = {
             ]
           },
           {
-            test: /\.global\.(scss|sass)$/,
-            use: ["style-loader", "css-loader", "sass-loader"]
-          },
-          {
-            test: /\.(scss|sass)$/,
+            test: /\.scss$/,
             use: [
-              "style-loader",
+              require.resolve("style-loader"),
               {
-                loader: "css-loader",
+                loader: require.resolve("css-loader"),
                 options: {
-                  modules: true,
-                  sourceMap: true,
-                  importLoaders: 2,
-                  localIdentName: "[name]__[local]___[hash:base64:5]"
+                  importLoaders: 1
                 }
               },
-              "sass-loader"
+              {
+                loader: require.resolve("sass-loader")
+              },
+              {
+                loader: require.resolve("postcss-loader"),
+                options: {
+                  // Necessary for external CSS imports to work
+                  // https://github.com/facebookincubator/create-react-app/issues/2677
+                  ident: "postcss",
+                  plugins: () => [
+                    require("postcss-flexbugs-fixes"),
+                    autoprefixer({
+                      browsers: [
+                        ">1%",
+                        "last 4 versions",
+                        "Firefox ESR",
+                        "not ie < 9" // React doesn't support IE8 anyway
+                      ],
+                      flexbox: "no-2009"
+                    })
+                  ]
+                }
+              }
             ]
           },
           // "file" loader makes sure those assets get served by WebpackDevServer.
@@ -215,12 +230,7 @@ module.exports = {
             // its runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [
-              /\.(js|jsx|mjs)$/,
-              /\.html$/,
-              /\.json$/,
-              /\.(scss|sass)$/
-            ],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             loader: require.resolve("file-loader"),
             options: {
               name: "static/media/[name].[hash:8].[ext]"
--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -208,24 +208,54 @@ module.exports = {
             // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
           {
-            test: /\.global\.(scss|sass)$/,
-            use: ["style-loader", "css-loader", "sass-loader"]
-          },
-          {
-            test: /\.(scss|sass)$/,
-            use: [
-              "style-loader",
-              {
-                loader: "css-loader",
-                options: {
-                  modules: true,
-                  sourceMap: true,
-                  importLoaders: 2,
-                  localIdentName: "[name]__[local]___[hash:base64:5]"
-                }
-              },
-              "sass-loader"
-            ]
+            test: /\.scss$/,
+            loader: ExtractTextPlugin.extract(
+              Object.assign(
+                {
+                  fallback: {
+                    loader: require.resolve("style-loader"),
+                    options: {
+                      hmr: false
+                    }
+                  },
+                  use: [
+                    {
+                      loader: require.resolve("css-loader"),
+                      options: {
+                        importLoaders: 1,
+                        minimize: true,
+                        sourceMap: shouldUseSourceMap
+                      }
+                    },
+                    {
+                      loader: require.resolve("sass-loader")
+                    },
+                    {
+                      loader: require.resolve("postcss-loader"),
+                      options: {
+                        // Necessary for external CSS imports to work
+                        // https://github.com/facebookincubator/create-react-app/issues/2677
+                        ident: "postcss",
+                        plugins: () => [
+                          require("postcss-flexbugs-fixes"),
+                          autoprefixer({
+                            browsers: [
+                              ">1%",
+                              "last 4 versions",
+                              "Firefox ESR",
+                              "not ie < 9" // React doesn't support IE8 anyway
+                            ],
+                            flexbox: "no-2009"
+                          })
+                        ]
+                      }
+                    }
+                  ]
+                },
+                extractTextPluginOptions
+              )
+            )
+            // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
           },
           // "file" loader makes sure assets end up in the `build` folder.
           // When you `import` an asset, you get its filename.
@@ -237,12 +267,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [
-              /\.(js|jsx|mjs)$/,
-              /\.html$/,
-              /\.json$/,
-              /\.(scss|sass)$/
-            ],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
               name: "static/media/[name].[hash:8].[ext]"
             }
